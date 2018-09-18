a124d3454bb1cdc3b4c30cf83fff1cb890ede49e
navneetlal <navneetlalg@gmail.com>
2018-09-18 12:19:29 +0530

    Material-ui--save

--- src/app/app.js
+++ src/app/app.js
@@ -1,21 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
 
-import MuiThemeProvider from '../../node_modules/@material-ui/core/styles/MuiThemeProvider';
-import ButtonMui from '../../node_modules/@material-ui/core/Button';
+// import ButtonMui from '@material-ui/core/Button';
+
 
 import "./styles/styles.scss";
-import "./components/header";
+import Navbar from "./components/header";
 import "./components/footer";
-
-console.log('Hello!');
 
 class App extends React.Component {
     render() {
         return (
-            <MuiThemeProvider>
-                <ButtonMui>Hello!</ButtonMui>
-            </MuiThemeProvider>
+                <Navbar/>
         );
     }
 }
---
+++ src/app/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavNeet from '@material-ui/core/AppBar';
+
+
+class Navigation extends React.Component{
+    render(){
+        return(
+            <NavNeet>
+                    Home
+            </NavNeet>
+        );
+    }
+}
+
+export default Navigation;
